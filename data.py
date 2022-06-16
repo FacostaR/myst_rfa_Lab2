701cc2c0f4fdebbea0c8afc7359f46866f3d238a
FacostaR <42416763+FacostaR@users.noreply.github.com>
2022-06-16 13:57:21 -0500

    Commit 1

--- data.py
+++ data.py
@@ -9,4 +9,32 @@
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
 
-dict_test = {'key_a': 'a', 'key_b': 'b'}
+import json
+import numpy as np
+import pandas as pd
+
+#Open JSON file
+
+f= open('orderbooks_05jul21.json')
+
+# Return JSON object as a Dictionary
+orderbooks_data = json.load(f)
+
+orderbooks_data.keys()
+
+bitfinex_ts= list(orderbooks_data['bitfinex'].keys())
+kraken_ts = list(orderbooks_data['kraken'].keys())
+
+ob_data= orderbooks_data['bitfinex']
+
+# Drop  None Keys
+ob_data= {i_key: i_value for i_key, i_value in ob_data.items() if i_value is not None}
+
+#Convert to DataFrame and rearange columns
+ob_data = {i_ob: pd.DataFrame(ob_data[i_ob])[['bid_size','bid','ask','ask_size']]
+                    if ob_data[i_ob] is not None else None for i_ob in list(ob_data.keys())}
+
+## Public Trades
+pt_data= pd.read_csv('btcusdt_binance.csv', index_col='timestamp')
+pt_data.index= pd.to_datetime(pt_data.index)
+pt_data.rename(columns= {'amount': 'volume'}, inplace='TRUE')
--- functions.py
+++ functions.py
@@ -8,3 +8,111 @@
 # -- repository: YOUR REPOSITORY URL                                                                     -- #
 # -- --------------------------------------------------------------------------------------------------- -- #
 """
+import numpy as np
+import data as dt
+import pandas as pd
+
+# Read input Data
+data_ob = dt.ob_data
+
+
+# =========================================================FUNCION============================================================= #
+
+def r_metrics_ob(data_ob: dict) -> dict:
+    """
+    Funcion para calcular metricas de un Orderbook
+
+    Parameters
+    ----------
+
+    data_ob:dict (default:None)
+
+        Datos  de entrada del Orderbook, es un diccionario con estructura siguiente:
+        'timestamp': objeto tipo timestamp reconocible por maquina e.g. pd.datatime()
+        'bid_size': Volumen de Bids
+        'bid': Niveles de Precio del Bid
+        'ask': Niveles de Precio del Ask
+        'ask_size': Volumen del Asks
+
+    Returns
+    -------
+
+    ret_data: dict
+
+        Diccionario con las metricas calculadas:
+        'No. Keys'
+        'OB Time'
+        'Time OB Updt'
+        'Spread'
+        'MidPrice'
+        'Price Levels'
+        'Bid Volume'
+        'Ask Volume'
+        'Total Volume'
+        'OrdB Imb'
+        'Weighted MP'
+        'VW Avg Price'
+
+    References
+    ----------
+    [1] https://www.geeksforgeeks.org/
+
+    """
+
+    # Time of Orderbook update
+    ob_ts = list(data_ob.keys())
+    l_ts = [pd.to_datetime(i_ts) for i_ts in ob_ts]
+    ob_m1 = np.median([l_ts[n_ts + 1] - l_ts[n_ts] for n_ts in range(0, len(l_ts) - 1)]).total_seconds() * 1000
+
+    # Spread
+    ob_m2 = [data_ob[ob_ts[i]]['ask'][0] - data_ob[ob_ts[i]]['bid'][0] for i in range(0, len(ob_ts))]
+
+    # Midprice
+    ob_m3 = [(data_ob[ob_ts[i]]['ask'][0] + data_ob[ob_ts[i]]['bid'][0]) * 0.5 for i in range(0, len(ob_ts))]
+
+    # No. Price Levels
+    ob_m4 = [data_ob[i_ts].shape[0] for i_ts in ob_ts]
+
+    # Bid_Volume, Ask_Volume, Total_Volume
+    ob_m5 = [np.round(data_ob[i_ts]['bid_size'].sum(), 6) for i_ts in ob_ts]
+    ob_m5_1 = [np.round(data_ob[i_ts]['ask_size'].sum(), 6) for i_ts in ob_ts]
+    ob_m5_2 = [np.round(data_ob[i_ts]['bid_size'].sum() + data_ob[i_ts]['ask_size'].sum(), 6) for i_ts in ob_ts]
+
+    # Orderbook Imbalance
+    # Sum(Bid Volume)/ Sum(Bid Volume , Ask Volume)
+    obim = [np.round(data_ob[i]['bid_size'].sum() / (data_ob[i]['bid_size'].sum() + data_ob[i]['ask_size'].sum()), 6)
+            for i in ob_ts]
+
+    # Weighted-Midprice
+    # Ask Volume/Sum(Bid Volume + Ask Volume)*Bid Price + Bid Volume/Sum(Bid Volume + Ask Volume)*Ask Price
+
+    w_midprice = [
+        (data_ob[i]['ask_size'].sum() / (data_ob[i]['bid_size'].sum() + data_ob[i]['ask_size'].sum())) * data_ob[i][
+            'bid'] +
+        (data_ob[i]['bid_size'].sum() / (data_ob[i]['bid_size'].sum() + data_ob[i]['ask_size'].sum())) * data_ob[i][
+            'ask'] for i in ob_ts]
+
+    # Volume-Weighted Average Price
+    # Sum((Bid price * Bid Volume + Ask price * Ask volume)/ Sum(Bid Volume + Ask Volume))
+    vw_midprice = [
+        ((data_ob[i]['bid'].sum() * data_ob[i]['bid_size'].sum()) + (data_ob[i]['ask'] * data_ob[i]['ask_size'].sum()))
+        / (data_ob[i]['bid_size'].sum() + data_ob[i]['ask_size'].sum()) for i in ob_ts]
+
+    ret_data = {'No. Keys': ob_ts, 'OB Time': l_ts,
+                'Time OB Updt': ob_m1, 'Spread': ob_m2,
+                'MidPrice': ob_m3, 'Price Levels': ob_m4,
+                'Bid Volume': ob_m5, 'Ask Volume': ob_m5_1,
+                'Total Volume': ob_m5_2, 'OrdB Imb': obim,
+                'Weighted MP': w_midprice, 'VW Avg Price': vw_midprice}
+
+    return ret_data
+
+
+def r_metrics_pt(data_pt: any) -> dict:
+    n_data_pt = data_pt['side'].resample('60T').count()
+    v_data_pt = data_pt['volume'].resample('60T').sum()
+    t_data_pt = v_data_pt.sum()
+
+    ret_data_pt = {'Numero de Trades': n_data_pt, 'Volumen de Trades': v_data_pt,
+                   'Monto Total de Trades': t_data_pt}
+    return ret_data_pt
